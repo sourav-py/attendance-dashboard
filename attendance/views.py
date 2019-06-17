ff34f695bd0c5980681642fc46f7b1d6dad1e745
sourav-py <s12souravin@gmail.com>
2019-06-17 23:51:23 +0530

    try

--- attendance/views.py
+++ attendance/views.py
@@ -453,7 +453,7 @@ def AttendanceModelEndPoint(request):
                 new_attendance_object.day31 = str(attendance)
             
             new_attendance_object.save()
-            update_attendance_object = Attendance.objects.get(user = CustomUser.objects.get(username = username),month = month)
+            update_attendance_object = Attendance.objects.filter(user = CustomUser.objects.get(username = username),month = month)
             update_attendance_object.day4 = 'Present'
             update_attendance_object.save()
             
